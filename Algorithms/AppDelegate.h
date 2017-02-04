f66fcfa14f4f9d9cd55c66a5dc9fc5a1c9b97b61
Maxime Boulat <maxime.boulat@gmail.com>
2017-02-03 19:27:46 -0800

    Initial Commit

---
+++ Algorithms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorithms
+//
+//  Created by Maxime Boulat on 2/3/17.
+//  Copyright © 2017 Maxime Boulat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorithms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorithms
+//
+//  Created by Maxime Boulat on 2/3/17.
+//  Copyright © 2017 Maxime Boulat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
